cd7a8096228fb4a06971335cdfdceb880e0fe91e
Fred Morris <consulting@m3047.net>
2022-07-31 16:55:56 -0700

    Sample configuration.

---
+++ peers/configuration-sample.py
@@ -0,0 +1,11 @@
+# If you have a domain (example.com), and you'd like to just specify the hosts
+# in it (foo) you set this to the domain. It's a convenience, kind of like a 
+# search list.
+#DOMAIN = 'example.com'
+
+# RKVDNS is always a subdomain of something. See the RKVDNS docs.
+#RKVDNS = 'redis.example.com'
+
+# The DNS server to use.
+#SERVER = '10.0.0.1'
+
