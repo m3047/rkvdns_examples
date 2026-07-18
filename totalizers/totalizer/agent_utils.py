6daafadb8cc89e7174f6d870717247a4bbe28f5c
Fred Morris <consulting@m3047.net>
2026-07-17 21:33:38 -0700

    Needs to import logging.

--- totalizers/totalizer/agent_utils.py
+++ totalizers/totalizer/agent_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (c) 2022 by Fred Morris Tacoma WA
+# Copyright (c) 2022,2026 by Fred Morris Tacoma WA
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 """Utilities for the Totalizer Agent."""
 
 from time import time
+import logging
 import re
 from ipaddress import ip_address
 
